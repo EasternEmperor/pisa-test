a64d8102f7bfd468042658d19caf580eba4b07f8
EasternEmperor <1908355690@qq.com>
2024-08-07 13:40:51 +0800

    获取用户答题记录筛选器

---
+++ src/main/java/com/research/pisatest/exception/PisatestException.java
@@ -0,0 +1,12 @@
+package com.research.pisatest.exception;
+
+/**
+ * @author zhongqilong
+ * @date 2024/8/7 13:28
+ * @description
+ */
+public class PisatestException extends RuntimeException{
+    public PisatestException(String message) {
+        super(message);
+    }
+}
