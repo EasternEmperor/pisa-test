95e77a5353c78b7674b87640d4cc731b46664ff3
EasternEmperor <1908355690@qq.com>
2024-08-19 21:36:00 +0800

    设计新答题数据保存表：p_pt_tickets_sale_data，p_pt_air_controller_data

--- build.sql
+++ build.sql
@@ -27,16 +27,39 @@ create table p_pt_user_answer (
     key uk_user_name_ith_answer(user_name, ith_answer)
 );
 insert into p_pt_user_answer(user_name) values('xiaoming20000101');
-alter table p_pt_user_answer add index uk_user_name_ith(user_name, ith_answer);
 
 create table p_pt_air_controller_data (
     id bigint(20) not null auto_increment,
+    html_name varchar(100) not null default '' comment '题目对应的前端文件名',
     user_name varchar(100) not null,
     ith_answer int(2) not null default 0,
     event varchar(50) not null default '' comment '操作事件',
     event_type varchar(10) not null default '' comment '事件类型',
     event_start_time timestamp not null default current_timestamp,
+    event_number int(10) not null default 0 comment '事件对应的顺序',
+    top_setting varchar(10) not null default 'NULL' comment 'top control设置的大小',
+    central_setting varchar(10) not null default 'NULL' comment 'central control设置的大小',
+    bottom_setting varchar(10) not null default 'NULL' comment 'bottom control设置的大小',
+    temp_value varchar(100) not null default 'NULL' comment '温度大小',
+    humid_value varchar(10) not null default 'NULL' comment '湿度大小',
     primary key(id),
     key uk_user_name_ith_answer(user_name, ith_answer)
 );
 insert into p_pt_air_controller_data(user_name) values('xiaoming20000101');
+
+create table p_pt_tickets_sale_data (
+    id bigint(20) not null auto_increment,
+    html_name varchar(100) not null default '' comment '题目对应的前端文件名',
+    user_name varchar(100) not null,
+    ith_answer int(2) not null default 0,
+    event_type varchar(10) not null default '' comment '事件类型',
+    event_start_time timestamp not null default current_timestamp,
+    event_number int(10) not null default 0 comment '事件对应的顺序',
+    network varchar(10) not null default 'NULL' comment '选择乘坐的交通网络',
+    fare_type varchar(10) not null default 'NULL' comment '全票还是优惠票',
+    ticket_type varchar(10) not null default 'NULL' comment '购票类型：单次票还是日票',
+    number_trips varchar(10) not null default 'NULL' comment '乘车次数',
+    primary key (id),
+    key uk_user_name_ith_answer(user_name, ith_answer)
+);
+insert into p_pt_tickets_sale_data(user_name) values('xiaoming20000101');
---
+++ src/main/java/com/research/pisatest/controller/AnswerDataController.java
@@ -0,0 +1,15 @@
+package com.research.pisatest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zhongqilong
+ * @date 2024/8/13 19:42
+ * @description
+ */
+@RestController
+@RequestMapping("/answerData")
+public class AnswerDataController {
+
+}
